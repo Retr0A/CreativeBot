66376edc11f2fec95f6663812cf6428ace343956
Alexander Buchkov <92212390+Retr0A@users.noreply.github.com>
2022-03-20 00:08:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,82 +1,28 @@
 from random import random
 import discord
-from discord.ext import commands
 import json
 import os
 import random
 
-#os.chdir("C:\\Users\\Bratl\\Github\\CreativeBot")
-
-client = discord.Client()
-client = commands.Bot(command_prefix = "//")
-
-@client.command()
-async def balance(ctx):
-    await open_account(ctx.author)
-    user = ctx.author
-    users = await get_bank_data()
-
-    wallet_amt = users[str(user.id)]["wallet"]
-    bank_amt = users[str(user.id)]["bank"]
-
-    em = discord.Embed(title = f"{ctx.author.name}'s balance", color = discord.Color.green())
-    em.add_field(name = "Wallet balance", value = wallet_amt)
-    em.add_field(name = "Bank balance", value = bank_amt)
-    await ctx.send(embed = em)
-
-@client.command()
-async def beg(ctx):
-    await open_account(ctx.author)
-
-    users = await get_bank_data()
-    user = ctx.author
-
-    earnings = random.randrange(101)
-
-    await ctx.send(f"Someone gave you {earnings} coins!!")
-
-    users[str(user.id)]["wallet"] += earnings
-
-    with open("mainbank.json", "w") as f:
-        json.dump(users, f)
-
-async def open_account (user):
-    users = await get_bank_data()
-
-    if str(user.id) in users:
-        return False
-    else:
-        users[str(user.id)] = {}
-        users[str(user.id)]["wallet"] = 0
-        users[str(user.id)]["bank"] = 0
-
-    with open("bank.json", "w") as f:
-        json.dump(users, f)
-    return True
-
-async def get_bank_data():
-    with open("mainbank.json", "r") as f:
-        users = json.load(f)
-
-    return users
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+botPrefix = '//'
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('//secretspamsussy'):
+    if message.content.startswith(botPrefix + 'secretspamsussy'):
 
         while 1 == 1:
           await message.channel.send("@everyone")
 
     convertedMessage = '{0.author.mention}`s cash is: 1000$'.format(message)
 
-    #if message.content.startswith('//cash Retr0A'):
+    #if message.content.startswith(botPrefix + 'cash Retr0A'):
         #await message.channel.send(accounts.account1.username + '`s cash is: ' + accounts.account1.money)
 
         #accountName = accounts.account1.username
@@ -86,13 +32,13 @@ async def on_message(message):
 
         # await message.channel.send(convertedMessage)
 
-    #if message.content.startswith('//addMoneyDebug'):
+    #if message.content.startswith(botPrefix + 'addMoneyDebug'):
         #accounts.account1.addCash(10);
 
-    #if message.content.startswith('//rob'):
+    #if message.content.startswith(botPrefix + 'rob'):
         #await message.channel.send("You robbed somone :)")
 
-    if message.content.startswith('//help'):
+    if message.content.startswith(botPrefix + 'help'):
       embed=discord.Embed(title="Help Command", description="The prefix of CreativeBot is //.", color=0x00FF85)
       embed.set_thumbnail(url="https://www.memesmonkey.com/images/memesmonkey/6f/6fa2b13f83413c7294eab3060e054573.jpeg");
       
@@ -106,7 +52,7 @@ async def on_message(message):
 
       await message.channel.send(embed=embed)
 
-    if message.content.startswith('//rules'):
+    if message.content.startswith(botPrefix + 'rules'):
       embed=discord.Embed(title="RULES Command", description="The rules are very important to know.", color=0xc80000)
       embed.set_thumbnail(url="https://th.bing.com/th/id/R.1931ea674c875a53c30ec59b598b9ed3?rik=4ZKzGXf4B1NTmg&riu=http%3a%2f%2fimages.memes.com%2fmeme%2f664153&ehk=kmWqbWmpDHNPIlLt65RF1ShWgbn%2blZ9NjDZIYA1VRho%3d&risl=&pid=ImgRaw&r=0");
       
